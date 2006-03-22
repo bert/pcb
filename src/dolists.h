46c1bf7c591542b364973f1b80be5aaba70bbe3d
Dan McMahill <dan@mcmahill.net>
2006-03-22 03:34:51 +0000

    add missing RCS Id

--- src/dolists.h
+++ src/dolists.h
@@ -1,3 +1,5 @@
+/* $Id$ */
+
 #undef REGISTER_ACTIONS
 #undef REGISTER_ATTRIBUTES
 #undef REGISTER_FLAGS
@@ -5,3 +7,4 @@
 #define REGISTER_ACTIONS(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
 #define REGISTER_ATTRIBUTES(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
 #define REGISTER_FLAGS(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
+
