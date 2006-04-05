00abf6a89a172ff326de154f9275814ff8a09b4b
Dan McMahill <dan@mcmahill.net>
2006-04-05 03:01:37 +0000

    Be a bit more careful with rouding issues when converting to pcb units. Hopefully this will help with the issue reported on the geda lists about 23.99 mil vias when the user thought they picked 24 mil ones.
    Patch posted on geda-user by DJ.

--- src/hid/gtk/gui.h
+++ src/hid/gtk/gui.h
@@ -81,7 +81,7 @@
 								((v) * 0.000254) : ((v) * 0.01))
 
 #define TO_PCB_UNITS(v)		(Settings.grid_units_mm ? \
-								((v) / 0.000254) : ((v) / 0.01))
+								((v) / 0.000254 + 0.5) : ((v) * 100.0 + 0.5))
 
 #define SIDE_X(x)   ((Settings.ShowSolderSide ? PCB->MaxWidth - (x) : (x)))
 
