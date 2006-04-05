248b1247696c3c48e1c1c5dc3aaae8f46e60623c
Harry Eaton <haceaton@users.sourceforge.net>
2006-04-05 03:03:37 +0000

    fixed pin/via bounding box to include thermal tails; bug reported by CL ----------------------------------------------------------------------

--- src/misc.c
+++ src/misc.c
@@ -158,7 +158,8 @@ SetPinBoundingBox (PinTypePtr Pin)
   /* the bounding box covers the extent of influence
    * so it must include the clearance values too
    */
-  width = (Pin->Clearance + 1) / 2 + (Pin->Thickness + 1) / 2;
+  width = (Pin->Clearance + Pin->Thickness + 1 +
+  (Pin->Thickness - Pin->DrillingHole)*PCB->ThermScale)/2;
   width = MAX (width, (Pin->Mask + 1) / 2);
   Pin->BoundingBox.X1 = Pin->X - width;
   Pin->BoundingBox.Y1 = Pin->Y - width;
