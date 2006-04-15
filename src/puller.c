5ac19ee54d669f4df76edec253383c50e00ee3d1
DJ Delorie <dj@delorie.com>
2006-04-15 03:32:06 +0000

    Document the puller action.

--- src/puller.c
+++ src/puller.c
@@ -220,6 +220,29 @@ find_pair (int Px, int Py)
   return 0;
 }
 
+
+static const char puller_syntax[] =
+"Puller()";
+
+static const char puller_help[] =
+"Pull an arc-line junction tight.";
+
+/* %start-doc actions Puller
+
+The @code{Puller()} action is a special-purpose optimization.  When
+invoked while the crosshair is over the junction of an arc and a line,
+it will adjust the arc's angle and the connecting line's enpoint such
+that the line intersects the arc at a tangent.  In the example below,
+the left side is ``before'' with the black target marking where to put
+the crosshair:
+
+@center @image{puller,,,Example of how puller works,png}
+
+The right side is ``after'' with the black target marking where the
+arc-line intersection was moved to.
+
+%end-doc */
+
 static int
 Puller (int argc, char **argv, int Ux, int Uy)
 {
@@ -329,7 +352,8 @@ Puller (int argc, char **argv, int Ux, int Uy)
 }
 
 HID_Action puller_action_list[] = {
-  {"Puller", "Click on a line-arc intersection or line segment", Puller}
+  {"Puller", "Click on a line-arc intersection or line segment", Puller,
+   puller_help, puller_syntax}
 };
 
 REGISTER_ACTIONS (puller_action_list)
