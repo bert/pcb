7aa9061094aeff7c8b66499f5b5cba0ca81ce58c
Ben Jackson <ben@ben.com>
2007-10-21 02:04:32 +0000

    Fix printf format warnings.

--- src/action.c
+++ src/action.c
@@ -740,7 +740,7 @@ GetFunctionID (String Ident)
       hash_initted = 1;
       if (HSIZE < ENTRIES (Functions) * 2)
 	{
-	  fprintf(stderr, "Error: function hash size too small (%d vs %ld at %s:%d)\n",
+	  fprintf(stderr, "Error: function hash size too small (%d vs %ud at %s:%d)\n",
 		  HSIZE, ENTRIES (Functions)*2, __FILE__,  __LINE__);
 	  exit(1);
 	}
@@ -748,7 +748,7 @@ GetFunctionID (String Ident)
 	{
 	  /* Change 'char' to 'int' and remove this when we get to 256
 	     strings to hash. */
-	  fprintf(stderr, "Error: function hash type too small (%d vs %ld at %s:%d)\n",
+	  fprintf(stderr, "Error: function hash type too small (%d vs %ud at %s:%d)\n",
 		  256, ENTRIES (Functions), __FILE__,  __LINE__);
 	  exit(1);
 	  
