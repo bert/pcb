884f94ff933c1b7d1176a4b61b3bf997bc9771bc
Dan McMahill <dan@mcmahill.net>
2008-01-12 00:44:37 +0000

    put in a check for autopoint version

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,21 @@
 CONFIG_SHELL=/bin/sh
 export CONFIG_SHELL
 
+echo "Checking autopoint version..."
+ver=`autopoint --version | awk '{print $NF; exit}'`
+ap_maj=`echo $ver | sed 's;\..*;;g'`
+ap_min=`echo $ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+ap_teeny=`echo $ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+echo "    $ver"
+
+case $ap_maj in
+	0)
+		if test $ap_min -lt 14 ; then
+			echo "You must have gettext >= 0.14.0 but you seem to have $ver"
+			exit
+		fi
+		;;
+esac
 echo "Running autopoint..."
 autopoint --force || exit 1
 
