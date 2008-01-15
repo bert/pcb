858765b213b80c88f52abe597805f3c7368f53db
Dan McMahill <dan@mcmahill.net>
2008-01-15 22:17:55 +0000

    Check for the version of intltool.  Also apply some fixups to the po/Makefile.in.in file created by intltoolize.

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,11 @@
 CONFIG_SHELL=/bin/sh
 export CONFIG_SHELL
 
+############################################################################
+#
+# autopoint (gettext)
+#
+
 echo "Checking autopoint version..."
 ver=`autopoint --version | awk '{print $NF; exit}'`
 ap_maj=`echo $ver | sed 's;\..*;;g'`
@@ -20,15 +25,57 @@ case $ap_maj in
 	0)
 		if test $ap_min -lt 14 ; then
 			echo "You must have gettext >= 0.14.0 but you seem to have $ver"
-			exit
+			exit 1
 		fi
 		;;
 esac
 echo "Running autopoint..."
 autopoint --force || exit 1
 
+############################################################################
+#
+# intltoolize (intltool)
+#
+
+echo "Checking intltoolize version..."
+it_ver=`intltoolize --version | awk '{print $NF; exit}'`
+it_maj=`echo $it_ver | sed 's;\..*;;g'`
+it_min=`echo $it_ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+it_teeny=`echo $it_ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+echo "    $it_ver"
+
+case $it_maj in
+	0)
+		if test $it_min -lt 36 ; then
+			echo "You must have intltool >= 0.36.0 but you seem to have $it_ver"
+			exit 1
+		fi
+		;;
+esac
 echo "Running intltoolize..."
 echo "no" | intltoolize --force --copy --automake || exit 1
+
+echo "Patching some intltoolize output"
+
+# both intltoolize and autopoint create a po/Makefile.in.in, this can't be good...
+# but intltoolize seems to have some bugs in it.  In particular, XGETTEXT and MSGFMT
+# are set in the Makefile but not passed down when calling MSGMERGE or GENPOT.
+# This defeats specifying the path to xgettext and msgfmt.  Also
+# we don't have a ChangeLog in the po/ directory right now so don't let
+# intltool try to include it in the distfile.
+
+mv po/Makefile.in.in po/Makefile.in.in.orig
+sed \
+	-e 's/^MSGMERGE *=/MSGMERGE = XGETTEXT=\${XGETTEXT} MSGFMT=\${MSGFMT} /g' \
+	-e 's/^GENPOT *=/GENPOT = XGETTEXT=\${XGETTEXT} MSGFMT=\${MSGFMT} /g' \
+	-e 's/ChangeLog//g' \
+	po/Makefile.in.in.orig > po/Makefile.in.in
+rm -f po/Makefile.in.in.orig
+
+############################################################################
+#
+# automake/autoconf stuff
+#
 
 echo "Running aclocal..."
 aclocal $ACLOCAL_FLAGS || exit 1
@@ -45,6 +92,11 @@ echo "Done with automake"
 echo "Running autoconf..."
 autoconf || exit 1
 echo "Done with autoconf"
+
+############################################################################
+#
+# finish up
+#
 
 echo "You must now run configure"
 
