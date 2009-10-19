515f6d3967aba780f056e79879eac1c1cca4fd1c
Peter Clifton <pcjc2@cam.ac.uk>
2009-10-19 17:47:55 +0100

    hid/common: Fix dereference bug in hid_find_action().
    We want to check context for being NULL, not *context.

--- src/hid/common/actions.c
+++ src/hid/common/actions.c
@@ -180,7 +180,7 @@ hid_find_action (const char *name, void **context)
 
   for (i = 0; i < n_actions; i++)
     if (strcasecmp (all_actions[i].action.name, name) == 0) {
-      if (*context != NULL)
+      if (context != NULL)
         *context = all_actions[i].context;
       return &(all_actions[i].action);
     }
