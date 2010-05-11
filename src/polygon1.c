4ba4e750b297d860a1264a59d3e418dfc9a6c635
Peter Clifton <pcjc2@cam.ac.uk>
2010-05-11 15:35:26 +0100

    Add comment explaining assumptions for poly_ContourInContour function
    Also, document its buggy boundary condition where the arbitrary point
    chosen to test happens to be a common node shared between two separate
    contours (which the test should return FALSE for).

--- src/polygon1.c
+++ src/polygon1.c
@@ -2254,6 +2254,11 @@ poly_M_CheckInside (POLYAREA * p, Vector v0)
   return FALSE;
 }
 
+/* NB: This function assumes the caller _knows_ the contours do not
+ *     intersect. If the contours intersect, the result is undefined.
+ *     This function can return an incorrect (positive) result if the
+ *     contours share a common node at the arbitrary point tested.
+ */
 int
 poly_ContourInContour (PLINE * poly, PLINE * inner)
 {
