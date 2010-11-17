11e0da5ae2e8704ec9a00e59780e904f81424276
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-11-17 19:59:40 +0100

    Added the pcb copyright notice.

--- src/hid/openscad/openscad.c
+++ src/hid/openscad/openscad.c
@@ -1,3 +1,33 @@
+/* $Id$ */
+
+/*
+ *                            COPYRIGHT
+ *
+ *  PCB, interactive printed circuit board design
+ *  Copyright (C) 1994,1995,1996 Thomas Nau
+ *  Copyright (C) 1997, 1998, 1999, 2000, 2001 Harry Eaton
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  Contact addresses for paper mail and Email:
+ *  Harry Eaton, 6697 Buttonhole Ct, Columbia, MD 21044, USA
+ *  haceaton@aplcomm.jhuapl.edu
+ *
+ */
+
+
 /*!
  * \file openscad.c
  * \author Copyright 2010 Bert Timmerman <bert.timmerman@xs4all.nl>
@@ -25,9 +55,6 @@
  * 
  * This exporter re-used the BOM HID code (partially) as a template.\n
  */
-
-
-/* $Id$ */
 
 
 #ifdef HAVE_CONFIG_H
