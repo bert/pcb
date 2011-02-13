08805bad16c623f5e1a1878f19ec085d306faec7
Krzysztof Kosciuszkiewicz <k.kosciuszkiewicz@gmail.com>
2011-02-14 00:02:48 +0100

    hid/gtk: handle CTRL-C in library window
    Handle CTRL-C keypress in library window and copy name of the selected
    component into the default GTK clipboard.
    
    The change facilitates workflow where one browses footprints in pcb and
    copies component names into gattrib or gschem.

--- src/hid/gtk/gui-library-window.c
+++ src/hid/gtk/gui-library-window.c
@@ -63,6 +63,8 @@
 #include "data.h"
 #include "set.h"
 
+#include <gdk/gdkkeysyms.h>
+
 #ifdef HAVE_LIBDMALLOC
 #include <dmalloc.h>
 #endif
@@ -304,6 +306,48 @@ tree_row_activated (GtkTreeView       *tree_view,
     gtk_tree_view_expand_row (tree_view, path, FALSE);
 }
 
+/*! \brief Handles CTRL-C keypress in the TreeView
+ *  \par Function Description
+ *  Keypress activation handler:
+ *  If CTRL-C is pressed, copy footprint name into the clipboard.
+ *
+ *  \param [in] tree_view The component treeview.
+ *  \param [in] event     The GdkEventKey with keypress info.
+ *  \param [in] user_data Not used.
+ *  \return TRUE if CTRL-C event was handled, FALSE otherwise.
+ */
+static gboolean
+tree_row_key_pressed (GtkTreeView *tree_view,
+                      GdkEventKey *event,
+                      gpointer     user_data)
+{
+  GtkTreeSelection *selection;
+  GtkTreeModel *model;
+  GtkTreeIter iter;
+  GtkClipboard *clipboard;
+  const gchar *compname;
+  guint default_mod_mask = gtk_accelerator_get_default_mod_mask();
+
+  /* Handle both lower- and uppercase `c' */
+  if (((event->state & default_mod_mask) != GDK_CONTROL_MASK)
+      || ((event->keyval != GDK_c) && (event->keyval != GDK_C)))
+    return FALSE;
+
+  selection = gtk_tree_view_get_selection (tree_view);
+  g_return_val_if_fail (selection != NULL, TRUE);
+
+  if (!gtk_tree_selection_get_selected (selection, &model, &iter))
+    return TRUE;
+
+  gtk_tree_model_get (model, &iter, MENU_NAME_COLUMN, &compname, -1);
+
+  clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+  g_return_val_if_fail (clipboard != NULL, TRUE);
+
+  gtk_clipboard_set_text (clipboard, compname, -1);
+
+  return TRUE;
+}
 
 /*! \brief Handles changes in the treeview selection.
  *  \par Function Description
@@ -604,6 +648,11 @@ create_lib_treeview (GhidLibraryWindow * library_window)
   g_signal_connect (libtreeview,
                     "row-activated",
                     G_CALLBACK (tree_row_activated),
+                    NULL);
+
+  g_signal_connect (libtreeview,
+                    "key-press-event",
+                    G_CALLBACK (tree_row_key_pressed),
                     NULL);
 
   /* connect callback to selection */
