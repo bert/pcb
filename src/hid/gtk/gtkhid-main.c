55210469f88f6e070e67341fcfa7d9183223d721
Peter Clifton <pcjc2@cam.ac.uk>
2011-03-26 23:36:30 +0000

    hid/gtk: Make benchmark() implementation suit multiple drawing models
    Invalidate and process updates, rather than calling redraw directly.
    This suits the coming GL renderer which will use an invalidate/expose
    model.

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -1562,8 +1562,8 @@ Benchmark (int argc, char **argv, int x, int y)
   time (&start);
   do
     {
-      hid_expose_callback (&ghid_hid, &region, 0);
-      gdk_display_sync (display);
+      ghid_invalidate_all ();
+      gdk_window_process_updates (gport->drawing_area->window, FALSE);
       time (&end);
       i++;
     }
