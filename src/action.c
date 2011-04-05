98aea009a75b662db43fe977d8bbdfb7a1241bec
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-06 00:59:00 +0100

    action.c: Remove commented out line of code
    Left from commit 089fbaf59c78fe75475db737e7e2827cd745d570
    (Initial C++ compatibility patch)

--- src/action.c
+++ src/action.c
@@ -2758,7 +2758,6 @@ ActionDisplay (int argc, char **argv, int childX, int childY)
 	  break;
 
 	case F_CycleCrosshair:
-	  //Crosshair.shape++;
 	  Crosshair.shape = CrosshairShapeIncrement(Crosshair.shape);
 	  if (Crosshair_Shapes_Number == Crosshair.shape)
 	    Crosshair.shape = Basic_Crosshair_Shape;
