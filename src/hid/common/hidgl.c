8478d4d8e661801d31cbc0c9bef1c13aab2403c6
Peter Clifton <pcjc2@cam.ac.uk>
2011-05-13 02:16:34 +0100

    hid/common/hidgl.c: Fix vertex array state preservation
    Fixes a bug with crosshair attached objects not being visible
    when the grid was rendered.
    
    Reported-by: Colin D Bennett <colin@gibibit.com>
    Closes-bug: lp-780958

--- src/hid/common/hidgl.c
+++ src/hid/common/hidgl.c
@@ -66,8 +66,6 @@ float global_depth = 0;
 void
 hidgl_init_triangle_array (triangle_buffer *buffer)
 {
-  glEnableClientState (GL_VERTEX_ARRAY);
-  glVertexPointer (3, GL_FLOAT, 0, buffer->triangle_array);
   buffer->triangle_count = 0;
   buffer->coord_comp_count = 0;
 }
@@ -78,7 +76,11 @@ hidgl_flush_triangles (triangle_buffer *buffer)
   if (buffer->triangle_count == 0)
     return;
 
+  glEnableClientState (GL_VERTEX_ARRAY);
+  glVertexPointer (3, GL_FLOAT, 0, buffer->triangle_array);
   glDrawArrays (GL_TRIANGLES, 0, buffer->triangle_count * 3);
+  glDisableClientState (GL_VERTEX_ARRAY);
+
   buffer->triangle_count = 0;
   buffer->coord_comp_count = 0;
 }
