596fa244e208faf3e0e81a7309e73d300c34a6d1
Peter Clifton <pcjc2@cam.ac.uk>
2011-09-06 01:18:25 +0100

    Revert "hid/gtk: Avoid the deprecated gdk_drawable_get_display()"
    This reverts commit c6d6ca16e58b5c7f2248c21e3248825f54e52374.
    
    Turns out the replacement API I used was only introduced in GTK 2.24,
    which is still a little new for us here.

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -1542,8 +1542,9 @@ Benchmark (int argc, char **argv, Coord x, Coord y)
 {
   int i = 0;
   time_t start, end;
-  GdkWindow *window = gtk_widget_get_window (gport->drawing_area);
-  GdkDisplay *display = gdk_window_get_display (window);
+  GdkDisplay *display;
+
+  display = gdk_drawable_get_display (gport->drawable);
 
   gdk_display_sync (display);
   time (&start);
