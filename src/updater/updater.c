ce18e8ca829d2c1982b69ae68481418284387e06
Bert Timmerman <bert.timmerman@xs4all.nl>
2011-12-10 09:13:04 +0100

    Added updater_get_file() and updater_fwrite().
    updater_get_file() can be used  to get a single file from an FTP server.
    It delays the actual destination file creation until the first write
    callback so that it won't create an empty file in case the remote file
    doesn't exist or something else fails.
    updater_fwrite() is the first write callback.

--- src/updater/updater.c
+++ src/updater/updater.c
@@ -29,6 +29,68 @@
 #include <curl/easy.h>
 
 
+struct FtpFile
+{
+  const char *filename;
+  FILE *stream;
+};
+
+ 
+static size_t
+updater_fwrite (void *buffer, size_t size, size_t nmemb, void *stream)
+{
+  struct FtpFile *out= (struct FtpFile *) stream;
+  if (out && !out->stream)
+  {
+    /* open file for writing */ 
+    out->stream = fopen (out->filename, "wb");
+    if (!out->stream)
+      return -1; /* failure, can't open file to write */ 
+  }
+  return fwrite (buffer, size, nmemb, out->stream);
+}
+
+
+int
+updater_get_file (char *filename)
+{
+  struct FtpFile ftpfile =
+  {
+    "pcb-latest.tar.gz", /* name to store the file as if successful */ 
+    NULL
+  };
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init ();
+  if (curl)
+  {
+    curl_easy_setopt (curl, CURLOPT_URL, filename);
+    /* Define our callback to get called when there's data to be written */ 
+    curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, updater_fwrite);
+    /* Set a pointer to our struct to pass to the callback */ 
+    curl_easy_setopt (curl, CURLOPT_WRITEDATA, &ftpfile);
+    /* Switch on full protocol/debug output */ 
+    curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L);
+    res = curl_easy_perform (curl);
+    curl_easy_cleanup (curl);
+    if (CURLE_OK != res)
+    {
+      fprintf (stderr, "curl told us %d\n", res);
+    }
+    else
+      {
+        fprintf (stderr, "received Content-Type: %s\n", res);
+      }
+  }
+  if (ftpfile.stream)
+  {
+    fclose (ftpfile.stream);
+  }
+  return 0;
+}
+
+
 int
 updater_get_latest_version_info (void)
 {
