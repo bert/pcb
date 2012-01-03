818ba7247eea5c2b137e55ed32011d11fb15cc7c
Peter Clifton <peter@clifton-electronics.co.uk>
2012-01-03 23:34:21 +0000

    Update the crosshair constraints after mirroring or rotating the buffer.
    Avoids a bug where the paste location of the mirrored or rotated buffer
    contents are improperly constrained based upon their old bounding box.

--- src/buffer.c
+++ src/buffer.c
@@ -1226,6 +1226,7 @@ RotateBuffer (BufferType *Buffer, BYTE Number)
   /* finally the origin and the bounding box */
   ROTATE (Buffer->X, Buffer->Y, Buffer->X, Buffer->Y, Number);
   RotateBoxLowLevel (&Buffer->BoundingBox, Buffer->X, Buffer->Y, Number);
+  SetCrosshairRangeToBuffer ();
 }
 
 static void
@@ -1360,6 +1361,7 @@ FreeRotateBuffer (BufferType *Buffer, Angle angle)
   ENDALL_LOOP;
 
   SetBufferBoundingBox (Buffer);
+  SetCrosshairRangeToBuffer ();
 }
 
 
@@ -1475,6 +1477,7 @@ MirrorBuffer (BufferType *Buffer)
   }
   ENDALL_LOOP;
   SetBufferBoundingBox (Buffer);
+  SetCrosshairRangeToBuffer ();
 }
 
 
@@ -1601,6 +1604,7 @@ SwapBuffer (BufferType *Buffer)
 	}
     }
   SetBufferBoundingBox (Buffer);
+  SetCrosshairRangeToBuffer ();
 }
 
 /* ----------------------------------------------------------------------
