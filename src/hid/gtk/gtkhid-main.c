a4c7e1238754711b9b10606e0adce43dd387652f
Peter Clifton <pcjc2@cam.ac.uk>
2015-01-08 20:52:23 +0000

    hid/gtk: Improve accuracy of "Benchmark" action somewhat
    Divide by the actual time taken, not exactly 10. (To get _good_
    accuracy we'd also need to use timers more accurate than 1s!)

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -1560,7 +1560,7 @@ Benchmark (int argc, char **argv, Coord x, Coord y)
     }
   while (end - start < 10);
 
-  printf (_("%g redraws per second\n"), i / 10.0);
+  printf (_("%g redraws per second\n"), (double)i / (double)(end-start));
 
   return 0;
 }
