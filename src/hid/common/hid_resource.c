a618bc53310a3a8e16f8e494170044d20ab67849
WileyECoyote <wileyhill@gmail.com>
2015-07-20 11:36:52 -0500

    Clean and reformat code

--- src/hid/common/hid_resource.c
+++ src/hid/common/hid_resource.c
@@ -6,13 +6,13 @@
 #include "resource.h"
 #include "hid/common/hid_resource.h"
 
-/* #define DEBUG_HID_RESOURCE */
+/*#define DEBUG_HID_RESOURCE */
 
-static int button_count;   // number of buttons we have actions for
-static int *button_nums;   // list of button numbers
-static int *mod_count;     // how many mods they have
-static unsigned *mods;     // mods, in order, one button after another
-static Resource** actions; // actions, in order, one button after another
+static int button_count;   /* number of buttons we have actions for */
+static int *button_nums;   /* list of button numbers */
+static int *mod_count;     /* how many mods they have */
+static unsigned *mods;     /* mods, in order, one button after another */
+static Resource** actions; /* actions, in order, one button after another */
 
 static Resource *
 res_wrap (char *value)
@@ -34,14 +34,18 @@ parse_mods (char *value)
     *s = tolower(*s);
 
   s = strstr(value, "mod");
-  if (s)
-    {
-      s += 3; // skip "mod" to get to number
-      errno = 0;
-      mod_num = strtol(s, (char**) NULL, 0);
-      if (!errno)
-        m |= M_Mod(mod_num);
+
+  if (s) {
+
+    s += 3; /* skip "mod" to get to number  */
+    errno = 0;
+    mod_num = strtol(s, (char**) NULL, 0);
+
+    if (!errno) {
+      m |= M_Mod(mod_num);
     }
+  }
+
   if (strstr(value, "shift"))
     m |= M_Shift;
   if (strstr(value, "ctrl"))
@@ -83,6 +87,7 @@ load_mouse_resource (const Resource *res)
 {
   int bi, mi, a;
   int action_count;
+
 #ifdef DEBUG_HID_RESOURCE
   fprintf(stderr, "note mouse resource:\n");
   resource_dump (res);
@@ -95,68 +100,70 @@ load_mouse_resource (const Resource *res)
 
   for (bi=0; bi<res->count; bi++) {
 
-      if (res->v[bi].value) {
-        action_count++;
-      }
+    if (res->v[bi].value) {
+      action_count++;
+    }
 
-      if (res->v[bi].subres) {
-        action_count += res->v[bi].subres->count;
-      }
+    if (res->v[bi].subres) {
+      action_count += res->v[bi].subres->count;
+    }
 
   }
+
   mods = (unsigned int *)malloc(action_count * sizeof(int));
   actions = (Resource **)malloc(action_count * sizeof(Resource*));
 
   a = 0;
-  for (bi=0; bi<res->count; bi++)
-    {
-      int button_num = button_name_to_num(res->v[bi].name);
 
-      if (button_num < 0)
-	continue;
+  for (bi=0; bi<res->count; bi++) {
 
-      button_nums[bi] = button_num;
-      mod_count[bi] = 0;
+    int button_num = button_name_to_num(res->v[bi].name);
 
-      if (res->v[bi].value)
-        {
-          mods[a] = 0;
-          actions[a++] = res_wrap (res->v[bi].value);
-          mod_count[bi] = 1;
-        }
+    if (button_num < 0)
+      continue;
 
-      if (res->v[bi].subres)
-	{
-	  Resource *m = res->v[bi].subres;
-          mod_count[bi] += m->count;
+    button_nums[bi] = button_num;
+    mod_count[bi] = 0;
 
-	  for (mi=0; mi<m->count; mi++, a++)
-	    {
-	      switch (resource_type (m->v[mi]))
-		{
-		case 1: /* subres only */
-                  mods[a] = 0;
-                  actions[a] = m->v[mi].subres;
-		  break;
+    if (res->v[bi].value) {
 
-		case 10: /* value only */
-                  mods[a] = 0;
-                  actions[a] = res_wrap (m->v[mi].value);
-		  break;
-
-		case 101: /* name = subres */
-		  mods[a] = parse_mods (m->v[mi].name);
-		  actions[a] = m->v[mi].subres;
-		  break;
-
-		case 110: /* name = value */
-		  mods[a] = parse_mods (m->v[mi].name);
-		  actions[a] = res_wrap (m->v[mi].value);
-		  break;
-		}
-	    }
-	}
+      mods[a] = 0;
+      actions[a++] = res_wrap (res->v[bi].value);
+      mod_count[bi] = 1;
     }
+
+    if (res->v[bi].subres) {
+
+      Resource *m = res->v[bi].subres;
+      mod_count[bi] += m->count;
+
+      for (mi=0; mi<m->count; mi++, a++) {
+
+        switch (resource_type (m->v[mi])) {
+
+          case 1: /* subres only */
+            mods[a] = 0;
+            actions[a] = m->v[mi].subres;
+            break;
+
+          case 10: /* value only */
+            mods[a] = 0;
+            actions[a] = res_wrap (m->v[mi].value);
+            break;
+
+          case 101: /* name = subres */
+            mods[a] = parse_mods (m->v[mi].name);
+            actions[a] = m->v[mi].subres;
+            break;
+
+          case 110: /* name = value */
+            mods[a] = parse_mods (m->v[mi].name);
+            actions[a] = res_wrap (m->v[mi].value);
+            break;
+        }
+      }
+    }
+  }
 }
 
 static Resource*
@@ -167,15 +174,15 @@ find_best_action (int button, int start, unsigned mod_mask)
   unsigned search_mask = mod_mask & ~M_Release;
   unsigned release_mask = mod_mask & M_Release;
 
-  // look for exact mod match
+  /* look for exact mod match  */
   for (i=start; i<start+count; i++)
     if (mods[i] == mod_mask) {
       return actions[i];
     }
 
   for (j=search_mask-1; j>=0; j--)
-    if ((j & search_mask) == j) // this would work
-      for (i=start; i<start+count; i++) // search for it
+    if ((j & search_mask) == j)         /* this would work */
+      for (i=start; i<start+count; i++) /* search for it  */
         if (mods[i] == (j | release_mask))
           return actions[i];
 
@@ -188,22 +195,29 @@ do_mouse_action (int button, int mod_mask)
   Resource *action = NULL;
   int bi, i, a;
 
-  // find the right set of actions;
+  /* find the right set of actions */
   a = 0;
+
   for (bi=0; bi<button_count && !action; bi++)
-    if (button_nums[bi] == button)
-      {
-        action = find_best_action(bi, a, mod_mask);
-        break;
+
+    if (button_nums[bi] == button) {
+
+      action = find_best_action(bi, a, mod_mask);
+      break;
+    }
+    else {
+      a += mod_count[bi]; /* skip this buttons actions */
+    }
+
+    if (!action) {
+      return;
+    }
+
+    for (i = 0; i < action->count; i++) {
+      if (action->v[i].value) {
+        if (hid_parse_actions (action->v[i].value)) {
+          return;
+        }
       }
-    else
-      a += mod_count[bi]; // skip this buttons actions
-
-  if (!action)
-    return;
-
-  for (i = 0; i < action->count; i++)
-    if (action->v[i].value)
-      if (hid_parse_actions (action->v[i].value))
-        return;
+    }
 }
