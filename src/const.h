9af2b11f13f3e6b9f1eb125d034bbbbda8840803
bert <bert.timmerman@xs4all.nl>
2015-09-07 18:01:31 +0200

    src/const.h: Converted plain comments into doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/const.h
+++ src/const.h
@@ -1,30 +1,31 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/const.h
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
+ * \brief Global source constants.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <hr>
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * PCB, interactive printed circuit board design
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Copyright (C) 1994,1995,1996 Thomas Nau
  *
- *  Contact addresses for paper mail and Email:
- *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
- *  Thomas.Nau@rz.uni-ulm.de
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- */
-
-/* global source constants
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Contact addresses for paper mail and Email:
+ * Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ * Thomas.Nau@rz.uni-ulm.de
  */
 
 #ifndef	PCB_CONST_H
@@ -52,9 +53,9 @@
 /* ---------------------------------------------------------------------------
  * misc constants
  */
-#define	MARK_SIZE		MIL_TO_COORD(50)	/* relative marker size */
-#define	UNDO_WARNING_SIZE	(1024*1024)	/* warning limit of undo */
-#define	USERMEDIANAME		"user defined"	/* label of default media */
+#define	MARK_SIZE		MIL_TO_COORD(50)	/*!< relative marker size */
+#define	UNDO_WARNING_SIZE	(1024*1024)	/*!< warning limit of undo */
+#define	USERMEDIANAME		"user defined"	/*!< label of default media */
 
 /* ---------------------------------------------------------------------------
  * some math constants
@@ -63,14 +64,14 @@
 #define	M_PI			3.14159265358979323846
 #endif
 #ifndef M_SQRT1_2
-#define M_SQRT1_2 		0.707106781	/* 1/sqrt(2) */
+#define M_SQRT1_2 		0.707106781	/*!< 1/sqrt(2) */
 #endif
 #define	M180			(M_PI/180.0)
 #define RAD_TO_DEG		(180.0/M_PI)
-#define	TAN_22_5_DEGREE_2	0.207106781	/* 0.5*tan(22.5) */
-#define COS_22_5_DEGREE		0.923879533	/* cos(22.5) */
-#define	TAN_30_DEGREE		0.577350269	/* tan(30) */
-#define	TAN_60_DEGREE		1.732050808	/* tan(60) */
+#define	TAN_22_5_DEGREE_2	0.207106781	/*!< 0.5*tan(22.5) */
+#define COS_22_5_DEGREE		0.923879533	/*!< cos(22.5) */
+#define	TAN_30_DEGREE		0.577350269	/*!< tan(30) */
+#define	TAN_60_DEGREE		1.732050808	/*!< tan(60) */
 #define LN_2_OVER_2		0.346573590
 
 /* PCB/physical unit conversions */
@@ -89,25 +90,25 @@
 /* ---------------------------------------------------------------------------
  * modes
  */
-#define	NO_MODE			0	/* no mode selected */
-#define	VIA_MODE		1	/* draw vias */
-#define	LINE_MODE		2	/* draw lines */
-#define	RECTANGLE_MODE		3	/* create rectangles */
-#define	POLYGON_MODE		4	/* draw filled polygons */
-#define	PASTEBUFFER_MODE	5	/* paste objects from buffer */
-#define	TEXT_MODE		6	/* create text objects */
-#define	ROTATE_MODE		102	/* rotate objects */
-#define	REMOVE_MODE		103	/* remove objects */
-#define	MOVE_MODE		104	/* move objects */
-#define	COPY_MODE		105	/* copy objects */
-#define	INSERTPOINT_MODE	106	/* insert point into line/polygon */
-#define	RUBBERBANDMOVE_MODE	107	/* move objects and attached lines */
-#define THERMAL_MODE            108	/* toggle thermal layer flag */
-#define ARC_MODE                109	/* draw arcs */
-#define ARROW_MODE		110	/* selection with arrow mode */
-#define PAN_MODE                0	/* same as no mode */
-#define LOCK_MODE               111	/* lock/unlock objects */
-#define	POLYGONHOLE_MODE	112	/* cut holes in filled polygons */
+#define	NO_MODE			0	/*!< no mode selected */
+#define	VIA_MODE		1	/*!< draw vias */
+#define	LINE_MODE		2	/*!< draw lines */
+#define	RECTANGLE_MODE		3	/*!< create rectangles */
+#define	POLYGON_MODE		4	/*!< draw filled polygons */
+#define	PASTEBUFFER_MODE	5	/*!< paste objects from buffer */
+#define	TEXT_MODE		6	/*!< create text objects */
+#define	ROTATE_MODE		102	/*!< rotate objects */
+#define	REMOVE_MODE		103	/*!< remove objects */
+#define	MOVE_MODE		104	/*!< move objects */
+#define	COPY_MODE		105	/*!< copy objects */
+#define	INSERTPOINT_MODE	106	/*!< insert point into line/polygon */
+#define	RUBBERBANDMOVE_MODE	107	/*!< move objects and attached lines */
+#define THERMAL_MODE            108	/*!< toggle thermal layer flag */
+#define ARC_MODE                109	/*!< draw arcs */
+#define ARROW_MODE		110	/*!< selection with arrow mode */
+#define PAN_MODE                0	/*!< same as no mode */
+#define LOCK_MODE               111	/*!< lock/unlock objects */
+#define	POLYGONHOLE_MODE	112	/*!< cut holes in filled polygons */
 
 /* ---------------------------------------------------------------------------
  * object flags
@@ -191,39 +192,39 @@ If set, this object has been as physically connected by @code{FindConnection()}.
 %end-doc */
 
 #define NOFLAG                  0x0000
-#define PINFLAG                 0x0001  /* is a pin */
-#define VIAFLAG                 0x0002  /* is a via */
-#define FOUNDFLAG               0x0004  /* used by 'FindConnection()' */
-#define HOLEFLAG                0x0008  /* pin or via is only a hole */
-#define NOPASTEFLAG             0x0008  /* pad should not receive
-                                           solderpaste.  This is to
-                                           support fiducials */
-#define RATFLAG                 0x0010  /* indicates line is a rat line */
-#define PININPOLYFLAG           0x0010  /* pin found inside poly - same as */
-                                        /* rat line since not used on lines */
-#define CLEARPOLYFLAG           0x0010  /* pins/vias clear these polygons */
-#define HIDENAMEFLAG            0x0010  /* hide the element name */
-#define DISPLAYNAMEFLAG         0x0020  /* display the names of pins/pads */
-                                        /* of an element */
-#define CLEARLINEFLAG           0x0020  /* line doesn't touch polygons */
-#define FULLPOLYFLAG            0x0020  /* full polygon is drawn (i.e. all parts instead of only the biggest one) */
-#define SELECTEDFLAG            0x0040  /* object has been selected */
-#define ONSOLDERFLAG            0x0080  /* element is on bottom side */
-#define AUTOFLAG                0x0080  /* line/via created by auto-router */
-#define SQUAREFLAG              0x0100  /* pin is square, not round */
-#define RUBBERENDFLAG           0x0200  /* indicates one end already rubber */
-                                        /* banding same as warn flag */
-                                        /* since pins/pads won't use it */
-#define WARNFLAG                0x0200  /* Warning for pin/via/pad */
-#define USETHERMALFLAG          0x0400  /* draw pin, via with thermal fingers */
-#define ONSILKFLAG              0x0400  /* old files use this to indicate silk */
-#define OCTAGONFLAG             0x0800  /* draw pin/via as octagon instead of round */
-#define DRCFLAG                 0x1000  /* flag like FOUND flag for DRC checking */
-#define LOCKFLAG                0x2000  /* object locked in place */
-#define EDGE2FLAG               0x4000  /* Padr.Point2 is closer to outside edge */
-                                        /* also pinout text for pins is vertical */
-#define VISITFLAG               0x8000  /* marker to avoid re-visiting an object */
-#define CONNECTEDFLAG          0x10000  /* flag like FOUND flag, but used to identify physically connected objects (not rats) */
+#define PINFLAG                 0x0001  /*!< is a pin */
+#define VIAFLAG                 0x0002  /*!< is a via */
+#define FOUNDFLAG               0x0004  /*!< used by 'FindConnection()' */
+#define HOLEFLAG                0x0008  /*!< pin or via is only a hole */
+#define NOPASTEFLAG             0x0008  /*!< pad should not receive
+                                             solderpaste.  This is to
+                                             support fiducials */
+#define RATFLAG                 0x0010  /*!< indicates line is a rat line */
+#define PININPOLYFLAG           0x0010  /*!< pin found inside poly - same as */
+                                        /*!< rat line since not used on lines */
+#define CLEARPOLYFLAG           0x0010  /*!< pins/vias clear these polygons */
+#define HIDENAMEFLAG            0x0010  /*!< hide the element name */
+#define DISPLAYNAMEFLAG         0x0020  /*!< display the names of pins/pads
+                                             of an element */
+#define CLEARLINEFLAG           0x0020  /*!< line doesn't touch polygons */
+#define FULLPOLYFLAG            0x0020  /*!< full polygon is drawn (i.e. all parts instead of only the biggest one) */
+#define SELECTEDFLAG            0x0040  /*!< object has been selected */
+#define ONSOLDERFLAG            0x0080  /*!< element is on bottom side */
+#define AUTOFLAG                0x0080  /*!< line/via created by auto-router */
+#define SQUAREFLAG              0x0100  /*!< pin is square, not round */
+#define RUBBERENDFLAG           0x0200  /*!< indicates one end already rubber
+                                             banding same as warn flag
+                                             since pins/pads won't use it */
+#define WARNFLAG                0x0200  /*!< Warning for pin/via/pad */
+#define USETHERMALFLAG          0x0400  /*!< draw pin, via with thermal fingers */
+#define ONSILKFLAG              0x0400  /*!< old files use this to indicate silk */
+#define OCTAGONFLAG             0x0800  /*!< draw pin/via as octagon instead of round */
+#define DRCFLAG                 0x1000  /*!< flag like FOUND flag for DRC checking */
+#define LOCKFLAG                0x2000  /*!< object locked in place */
+#define EDGE2FLAG               0x4000  /*!< Padr.Point2 is closer to outside edge
+                                             also pinout text for pins is vertical */
+#define VISITFLAG               0x8000  /*!< marker to avoid re-visiting an object */
+#define CONNECTEDFLAG          0x10000  /*!< flag like FOUND flag, but used to identify physically connected objects (not rats) */
 
 
 #define NOCOPY_FLAGS (FOUNDFLAG | CONNECTEDFLAG)
@@ -314,7 +315,7 @@ When set, element names are not drawn.
 /* ---------------------------------------------------------------------------
  * object types
  */
-#define	NO_TYPE			0x00000	/* no object */
+#define	NO_TYPE			0x00000	/*!< no object */
 #define	VIA_TYPE		0x00001
 #define	ELEMENT_TYPE		0x00002
 #define	LINE_TYPE		0x00004
@@ -322,23 +323,23 @@ When set, element names are not drawn.
 #define	TEXT_TYPE		0x00010
 #define RATLINE_TYPE		0x00020
 
-#define	PIN_TYPE		0x00100	/* objects that are part */
-#define	PAD_TYPE		0x00200	/* 'pin' of SMD element */
-#define	ELEMENTNAME_TYPE	0x00400	/* of others */
+#define	PIN_TYPE		0x00100	/*!< objects that are part */
+#define	PAD_TYPE		0x00200	/*!< 'pin' of SMD element */
+#define	ELEMENTNAME_TYPE	0x00400	/*!< of others */
 #define	POLYGONPOINT_TYPE	0x00800
 #define	LINEPOINT_TYPE		0x01000
 #define ELEMENTLINE_TYPE        0x02000
 #define ARC_TYPE                0x04000
 #define ELEMENTARC_TYPE		0x08000
 
-#define LOCKED_TYPE 		0x10000	/* used to tell search to include locked items. */
-#define NET_TYPE		0x20000 /* used to select whole net. */
+#define LOCKED_TYPE 		0x10000	/*!< used to tell search to include locked items. */
+#define NET_TYPE		0x20000 /*!< used to select whole net. */
 #define ARCPOINT_TYPE		0x40000
 
 #define PIN_TYPES     (VIA_TYPE | PIN_TYPE)
 #define LOCK_TYPES    (VIA_TYPE | LINE_TYPE | ARC_TYPE | POLYGON_TYPE | ELEMENT_TYPE \
                       | TEXT_TYPE | ELEMENTNAME_TYPE | LOCKED_TYPE)
 
-#define	ALL_TYPES		(~0)	/* all bits set */
+#define	ALL_TYPES		(~0)	/*!< all bits set */
 
 #endif
