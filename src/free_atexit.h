f740251f5c17f73f0d8c876065a6671ef84015b0
WileyECoyote <wileyhill@gmail.com>
2015-12-20 23:03:51 -0600

    src/free_atexit.[ch]: Bert: Converted plain comments into doxygen comments.

--- src/free_atexit.h
+++ src/free_atexit.h
@@ -1,9 +1,16 @@
-/* This tiny library is to assist cleaning up harmless memory leaks caused
-   by (growing) buffers allocated in static variables in functions. The
-   library provides leaky_ prefixed variants of the common allocation
-   routines. These wrappers will remember all pointers they return and
-   can free all memory used, at the end of the applocation.
-*/
+/*!
+ * \file src/free_atexit.c
+ *
+ * \brief .
+ *
+ * This tiny library is to assist cleaning up harmless memory leaks
+ * caused by (growing) buffers allocated in static variables in
+ * functions.\n
+ * The library provides leaky_ prefixed variants of the common
+ * allocation routines.\n
+ * These wrappers will remember all pointers they return and can free
+ * all memory used, at the end of the application.
+ */
 
 #include <stdlib.h>
 
@@ -15,19 +22,10 @@
 #define leaky_realloc(old_memory, size) realloc(old_memory, size)
 #else
 
-/* set up atexit() hook - can be avoided if leaky_uninit() is called by hand */
 void leaky_init (void);
-
-/* free all allocations */
 void leaky_uninit (void);
-
-/* allocate memory, remember the pointer and free it after exit from the application */
 void *leaky_malloc (size_t size);
-
-/* same as leaky_malloc but this one wraps calloc() */
 void *leaky_calloc (size_t nmemb, size_t size);
-
-/* reallocate memory, remember the new pointer and free it after exit from the application */
 void *leaky_realloc (void* old_memory, size_t size);
 
 
