7900d5e3142bab3694f9aa6a75953541acfe9d01
Anton Dubniak <twister@tfsoft.org.ua>
2016-01-04 01:00:37 +0200

    Add measurement units to route-styles string in preferences file.
    This avoids unit conversion to cmil on save of preferences, and
    avoids the rounding errors associated with that.

--- src/misc.c
+++ src/misc.c
@@ -945,7 +945,7 @@ make_route_string (RouteStyleType rs[], int n_styles)
 
   for (i = 0; i < n_styles; ++i)
     {
-      char *r_string = pcb_g_strdup_printf ("%s,%mc,%mc,%mc,%mc", rs[i].Name,
+      char *r_string = pcb_g_strdup_printf ("%s,%$mr,%$mr,%$mr,%$mr", rs[i].Name,
                                             rs[i].Thick, rs[i].Diameter,
                                             rs[i].Hole, rs[i].Keepaway);
       if (i > 0)
