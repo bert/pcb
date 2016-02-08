822d0ceee2e4ae68c2374fc275d3b44bf2703b47
bert <bert.timmerman@xs4all.nl>
2016-02-08 21:36:20 +0100

    src/lrealpath.[ch]: Converted plain comments into doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/lrealpath.h
+++ src/lrealpath.h
@@ -1,7 +1,12 @@
+/*!
+ * \file src/lrealpath.h
+ *
+ * \brief Prototypes for Libiberty realpath.
+ */
+
 #ifndef PCB_LREALPATH_H
 #define PCH_LREALPATH_H
 
-/* A well-defined realpath () that is always compiled in.  */
 char *lrealpath (const char *);
 
 #endif /* PCB_LREALPATH_H */
