25f62ab5c4038fae7b57846432d6b3556b701bcb
bert <bert.timmerman@xs4all.nl>
2016-04-30 06:37:59 +0200

    src/rats.[ch]: Converted plain comments into doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rats.c
+++ src/rats.c
@@ -1,26 +1,36 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/rats.c
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
- * 
- *  This module, rats.c, was written and is Copyright (C) 1997 by harry eaton
- *  this module is also subject to the GNU GPL as described below
+ * \brief Rats nest routines.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <hr>
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * PCB, interactive printed circuit board design
  *
+ * Copyright (C) 1994,1995,1996 Thomas Nau
+ *
+ * Copyright (C) 1997, harry eaton
+ *
+ * This module, rats.c, was written and is Copyright (C) 1997 by
+ * harry eaton.
+ *
+ * This module is also subject to the GNU GPL as described below.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 /* Change History:
@@ -30,8 +40,6 @@
  * support for netlist window 10/24/98
  */
 
-/* rats nest routines
- */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -78,12 +86,14 @@ static void TransferNet (NetListType *, NetType *, NetType *);
 static bool badnet = false;
 static Cardinal top_group, bottom_group;	/* layer group holding top/bottom side */
 
-/* ---------------------------------------------------------------------------
- * parse a connection description from a string
- * puts the element name in the string and the pin number in
- * the number.  If a valid connection is found, it returns the
- * number of characters processed from the string, otherwise
- * it returns 0
+/*!
+ * \brief Parse a connection description from a string.
+ *
+ * Puts the element name in the string and the pin number in
+ * the number.
+ *
+ * \return If a valid connection is found, it returns the number of characters
+ * processed from the string, otherwise it returns 0.
  */
 static bool
 ParseConnection (char *InString, char *ElementName, char *PinNum)
@@ -111,8 +121,8 @@ ParseConnection (char *InString, char *ElementName, char *PinNum)
     }
 }
 
-/* ---------------------------------------------------------------------------
- * Find a particular pad from an element name and pin number
+/*!
+ * \brief Find a particular pad from an element name and pin number.
  */
 static bool
 FindPad (char *ElementName, char *PinNum, ConnectionType * conn, bool Same)
@@ -170,9 +180,10 @@ FindPad (char *ElementName, char *PinNum, ConnectionType * conn, bool Same)
   return false;
 }
 
-/*--------------------------------------------------------------------------
- * parse a netlist menu entry and locate the corresponding pad
- * returns true if found, and fills in Connection information
+/*!
+ * \brief Parse a netlist menu entry and locate the corresponding pad.
+ *
+ * \return true if found, and fills in Connection information.
  */
 bool
 SeekPad (LibraryEntryType * entry, ConnectionType * conn, bool Same)
@@ -208,10 +219,9 @@ SeekPad (LibraryEntryType * entry, ConnectionType * conn, bool Same)
   return (false);
 }
 
-/* ---------------------------------------------------------------------------
- * Read the library-netlist build a true Netlist structure
+/*!
+ * \brief Read the library-netlist build a true Netlist structure.
  */
-
 NetListType *
 ProcNetlist (LibraryType *net_menu)
 {
@@ -327,9 +337,9 @@ ProcNetlist (LibraryType *net_menu)
   return (Wantlist);
 }
 
-/*
- * copy all connections from one net into another
- * and then remove the first net from its netlist
+/*!
+ * \brief Copy all connections from one net into another and then remove
+ * the first net from its netlist.
  */
 static void
 TransferNet (NetListType *Netl, NetType *SourceNet, NetType *DestNet)
@@ -441,11 +451,13 @@ CheckShorts (LibraryMenuType *theNet)
 }
 
 
-/* ---------------------------------------------------------------------------
- * Determine existing interconnections of the net and gather into sub-nets
+/*!
+ * \brief Determine existing interconnections of the net and gather into
+ * sub-nets.
  *
- * initially the netlist has each connection in its own individual net
- * afterwards there can be many fewer nets with multiple connections each
+ * Initially the netlist has each connection in its own individual net
+ * afterwards there can be many fewer nets with multiple connections
+ * each.
  */
 static bool
 GatherSubnets (NetListType *Netl, bool NoWarn, bool AndRats)
@@ -541,16 +553,40 @@ GatherSubnets (NetListType *Netl, bool NoWarn, bool AndRats)
   return (Warned);
 }
 
-/* ---------------------------------------------------------------------------
- * Draw a rat net (tree) having the shortest lines
- * this also frees the subnet memory as they are consumed
+/*!
+ * \brief Draw a rat net (tree) having the shortest lines.
  *
- * Note that the Netl we are passed is NOT the main netlist - it's the
- * connectivity for ONE net.  It represents the CURRENT connectivity
- * state for the net, with each Netl->Net[N] representing one
- * copper-connected subset of the net.
+ * This also frees the subnet memory as they are consumed.
+ *
+ * \note The \c Netl we are passed is NOT the main netlist - it's the
+ * connectivity for ONE net.
+ * It represents the CURRENT connectivity state for the net, with each
+ * Netl->Net[N] representing one copper-connected subset of the net.
+ *
+ * Everything inside the NetList Netl should be connected together.
+ *
+ * Each Net in \c Netl is a group of Connections which are already
+ * connected together somehow, either by real wires or by rats we've
+ * already drawn.
+ *
+ * Each Connection is a vertex within that blob of connected items.
+ *
+ * This loop finds the closest vertex pairs between each blob and draws
+ * rats that merge the blobs until there's just one big blob.
+ *
+ * Just to clarify, with some examples:
+ *
+ * Each \c Netl is one full net from a netlist, like from gnetlist.
+ *
+ * Each Netl->Net[N] is a subset of that net that's already
+ * physically connected on the pcb.
+ *
+ * So a new design with no traces yet, would have a huge list of Net[N],
+ * each with one pin in it.
+ *
+ * A fully routed design would have one Net[N] with all the pins
+ * (for that net) in it.
  */
-
 static bool
 DrawShortestRats (NetListType *Netl, void (*funcp) (register ConnectionType *, register ConnectionType *, register RouteStyleType *))
 {
@@ -568,28 +604,6 @@ DrawShortestRats (NetListType *Netl, void (*funcp) (register ConnectionType *, r
    */
   if (!Netl || Netl->NetN < 1)
     return false;
-
-  /*
-   * Everything inside the NetList Netl should be connected together.
-   * Each Net in Netl is a group of Connections which are already
-   * connected together somehow, either by real wires or by rats we've
-   * already drawn.  Each Connection is a vertex within that blob of
-   * connected items.  This loop finds the closest vertex pairs between
-   * each blob and draws rats that merge the blobs until there's just
-   * one big blob.
-   *
-   * Just to clarify, with some examples:
-   *
-   * Each Netl is one full net from a netlist, like from gnetlist.
-   * Each Netl->Net[N] is a subset of that net that's already
-   * physically connected on the pcb.
-   *
-   * So a new design with no traces yet, would have a huge list of Net[N],
-   * each with one pin in it.
-   *
-   * A fully routed design would have one Net[N] with all the pins
-   * (for that net) in it.
-   */
 
   /*
    * We keep doing this do/while loop until everything's connected.
@@ -716,9 +730,12 @@ DrawShortestRats (NetListType *Netl, void (*funcp) (register ConnectionType *, r
 }
 
 
-/* ---------------------------------------------------------------------------
- *  AddAllRats puts the rats nest into the layout from the loaded netlist
- *  if SelectedOnly is true, it will only draw rats to selected pins and pads
+/*!
+ * \brief AddAllRats puts the rats nest into the layout from the loaded
+ * netlist.
+ *
+ * If SelectedOnly is true, it will only draw rats to selected pins and
+ * pads.
  */
 bool
 AddAllRats (bool SelectedOnly, void (*funcp) (register ConnectionType *, register ConnectionType *, register RouteStyleType *))
@@ -809,9 +826,11 @@ AddAllRats (bool SelectedOnly, void (*funcp) (register ConnectionType *, registe
   return (false);
 }
 
-/* XXX: This is copied in large part from AddAllRats above; for
+/*!
+ * \todo This is copied in large part from AddAllRats above; for
  * maintainability, AddAllRats probably wants to be tweaked to use this
- * version of the code so that we don't have duplication. */
+ * version of the code so that we don't have duplication.
+ */
 NetListListType
 CollectSubnets (bool SelectedOnly)
 {
@@ -868,9 +887,9 @@ CollectSubnets (bool SelectedOnly)
   return result;
 }
 
-/*
- * Check to see if a particular name is the name of an already existing rats
- * line
+/*!
+ * \brief Check to see if a particular name is the name of an already
+ * existing rats line.
  */
 static int
 rat_used (char *name)
@@ -888,9 +907,10 @@ rat_used (char *name)
   return 0;
 }
 
-  /* These next two functions moved from the original netlist.c as part of the
-     |  gui code separation for the Gtk port.
-   */
+/*!
+ * \brief This function is moved from the original netlist.c as
+ * part of the gui code separation for the Gtk port.
+ */
 RatType *
 AddNet (void)
 {
@@ -997,7 +1017,10 @@ ratIt:
 			group1, group2, Settings.RatThickness, NoFlags ()));
 }
 
-
+/*!
+ * \brief This function is moved from the original netlist.c as
+ * part of the gui code separation for the Gtk port.
+ */
 char *
 ConnectionName (int type, void *ptr1, void *ptr2)
 {
--- src/rats.h
+++ src/rats.h
@@ -1,32 +1,37 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/rats.h
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
+ * \brief Prototypes for rats routines.
  *
- *	rats.c, rats.h Copyright (C) 1997, harry eaton
+ * <hr>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * PCB, interactive printed circuit board design
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Copyright (C) 1994,1995,1996 Thomas Nau
  *
- *  Contact addresses for paper mail and Email:
- *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
- *  Thomas.Nau@rz.uni-ulm.de
+ * Copyright (C) 1997, harry eaton
  *
- */
-
-/* prototypes for rats routines
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Contact addresses for paper mail and Email:
+ *
+ * Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *
+ * Thomas.Nau@rz.uni-ulm.de
  */
 
 #ifndef	PCB_RATS_H
