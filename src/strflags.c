40565377ab141b80769a7d79dec4c5b3fb7cbc60
bert <bert.timmerman@xs4all.nl>
2016-05-16 09:45:37 +0200

    src/strflags.[ch]: Converted plain comments into doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/strflags.c
+++ src/strflags.c
@@ -1,27 +1,39 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/strflags.c
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 2005 DJ Delorie
+ * \brief strflags.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * The purpose of this interface is to make the file format able to
+ * handle more than 32 flags, and to hide the internal details of
+ * flags from the file format.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * <hr>
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  Contact addresses for paper mail and Email:
- *  DJ Delorie, 334 North Road, Deerfield NH 03037-1110, USA
- *  dj@delorie.com
+ * PCB, interactive printed circuit board design
  *
+ * Copyright (C) 2005 DJ Delorie
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Contact addresses for paper mail and Email:
+ *
+ * DJ Delorie, 334 North Road, Deerfield NH 03037-1110, USA
+ *
+ * dj@delorie.com
  */
 
 #ifdef HAVE_CONFIG_H
@@ -53,7 +65,8 @@ typedef struct
   FlagType Flags;
 } FlagHolder;
 
-/* Be careful to list more specific flags first, followed by general
+/*!
+ * Be careful to list more specific flags first, followed by general
  * flags, when two flags use the same bit.  For example, "onsolder" is
  * for elements only, while "auto" is for everything else.  They use
  * the same bit, but onsolder is listed first so that elements will
@@ -61,22 +74,15 @@ typedef struct
  *
  * Thermals are handled separately, as they're layer-selective.
  */
-
 typedef struct
 {
-
-  /* This is the bit that we're setting.  */
-  int mask;
-
-  /* The name used in the output file.  */
-  char *name;
+  int mask; /*!< This is the bit that we're setting. */
+  char *name; /*!< The name used in the output file. */
   int nlen;
 #define N(x) x, sizeof(x)-1
 
-  /* If set, this entry won't be output unless the object type is one
-     of these.  */
-  int object_types;
-
+  int object_types; /*!< If set, this entry won't be output unless the
+    * object type is one of these.  */
 } FlagBitsType;
 
 static FlagBitsType object_flagbits[] = {
@@ -178,16 +184,20 @@ uninit_strflags_buf (void)
     }
 }
 
-/*
- * This set of routines manages a list of layer-specific flags.
- * Callers should call grow_layer_list(0) to reset the list, and
- * set_layer_list(layer,1) to set bits in the layer list.  The results
- * are stored in layers[], which has num_layers valid entries.
- */
-
 static char *layers = 0;
 static int max_layers = 0, num_layers = 0;
 
+/*!
+ * \brief Grow layer list.
+ *
+ * This routine manages a list of layer-specific flags.
+ *
+ * Callers should call grow_layer_list(0) to reset the list, and
+ * set_layer_list(layer,1) to set bits in the layer list.
+ *
+ * The results are stored in layers[], which has \c num_layers valid
+ * entries.
+ */
 static void
 grow_layer_list (int num)
 {
@@ -218,6 +228,17 @@ uninit_strflags_layerlist (void)
     }
 }
 
+/*!
+ * \brief Set layer list.
+ *
+ * This routine manages a list of layer-specific flags.
+ *
+ * Callers should call grow_layer_list(0) to reset the list, and
+ * set_layer_list(layer,1) to set bits in the layer list.
+ *
+ * The results are stored in layers[], which has \c num_layers valid
+ * entries.
+ */
 static inline void
 set_layer_list (int layer, int v)
 {
@@ -226,23 +247,24 @@ set_layer_list (int layer, int v)
   layers[layer] = v;
 }
 
-/*
- * These next two convert between layer lists and strings.
- * parse_layer_list() is passed a pointer to a string, and parses a
- * list of integer which reflect layers to be flagged.  It returns a
- * pointer to the first character following the list.  The syntax of
- * the list is a paren-surrounded, comma-separated list of integers
- * and/or pairs of integers separated by a dash (like "(1,2,3-7)").
- * Spaces and other punctuation are not allowed.  The results are
- * stored in layers[] defined above.
+/*!
+ * \brief Parse layer list.
  *
- * print_layer_list() does the opposite - it uses the flags set in
- * layers[] to build a string that represents them, using the syntax
- * above.
- * 
+ * parse_layer_list() is passed a pointer to a string, and parses a
+ * list of integer which reflect layers to be flagged.
+ *
+ * It returns a pointer to the first character following the list.
+ *
+ * The syntax of the list is a paren-surrounded, comma-separated list of
+ * integers and/or pairs of integers separated by a dash
+ * (like "(1,2,3-7)").
+ *
+ * Spaces and other punctuation are not allowed.
+ *
+ * The results are stored in \c layers[].
+ *
+ * \return a pointer to the first character past the list.
  */
-
-/* Returns a pointer to the first character past the list. */
 static const char *
 parse_layer_list (const char *bp, int (*error) (const char *))
 {
@@ -295,7 +317,9 @@ parse_layer_list (const char *bp, int (*error) (const char *))
   return bp;
 }
 
-/* Number of character the value "i" requires when printed. */
+/*!
+ * \brief Number of character the value "i" requires when printed.
+ */
 static int
 printed_int_length (int i, int j)
 {
@@ -311,8 +335,21 @@ printed_int_length (int i, int j)
   return rv + (j ? 1 : 0);
 }
 
-/* Returns a pointer to an internal buffer which is overwritten with
-   each new call.  */
+/*!
+ * \brief Print layer list.
+ *
+ * print_layer_list() uses the flags set in layers[] to build a string
+ * that represents them, using the syntax described below.
+ *
+ * The syntax of the list is a paren-surrounded, comma-separated list of
+ * integers and/or pairs of integers separated by a dash
+ * (like "(1,2,3-7)").
+ *
+ * Spaces and other punctuation are not allowed.
+ *
+ * \return a pointer to an internal buffer which is overwritten with
+ * each new call.
+ */
 static char *
 print_layer_list ()
 {
@@ -375,13 +412,6 @@ print_layer_list ()
   *bp = 0;
   return buf;
 }
-
-/*
- * Ok, now the two entry points to this file.  The first, string_to_flags,
- * is passed a string (usually from parse_y.y) and returns a "set of flags".
- *
- * Currently, there is no error handling :-P
- */
 
 static int
 error_ignore (const char *msg)
@@ -453,6 +483,15 @@ common_string_to_flags (const char *flagstring,
   return rv.Flags;
 }
 
+/*!
+ * \brief Convert strings to flags.
+ *
+ * When passed a string, parse it and return an appropriate set of
+ * flags.
+ *
+ * Errors cause error() to be called with a suitable message;
+ * if error is NULL, errors are ignored.
+ */
 FlagType
 string_to_flags (const char *flagstring,
 		 int (*error) (const char *msg))
@@ -463,6 +502,15 @@ string_to_flags (const char *flagstring,
 				 ENTRIES (object_flagbits));
 }
 
+/*!
+ * \brief Convert strings to PCB flags.
+ *
+ * When passed a string, parse it and return an appropriate set of
+ * flags.
+ *
+ * Errors cause error() to be called with a suitable message;
+ * if error is NULL, errors are ignored.
+ */
 FlagType
 string_to_pcbflags (const char *flagstring,
 		    int (*error) (const char *msg))
@@ -473,17 +521,21 @@ string_to_pcbflags (const char *flagstring,
 				 ENTRIES (pcb_flagbits));
 }
 
-/*
- * Given a set of flags for a given type of object, return a string
- * which reflects those flags.  The only requirement is that this
- * string be parseable by string_to_flags.
+/*!
+ * \brief Common flags converted to strings.
  *
- * Note that this function knows a little about what kinds of flags
+ * Given a set of flags for a given type of object, return a string
+ * which reflects those flags.
+ *
+ * The only requirement is that this string be parseable by string_to_flags.
+ *
+ * \note This function knows a little about what kinds of flags
  * will be automatically set by parsing, so it won't (for example)
  * include the "via" flag for VIA_TYPEs because it knows those get
  * forcibly set when vias are parsed.
+ *
+ * \warning Currently, there is no error handling :-P
  */
-
 static char *
 common_flags_to_string (FlagType flags,
 			int object_type,
@@ -564,6 +616,14 @@ common_flags_to_string (FlagType flags,
   return buf;
 }
 
+/*!
+ * \brief Object flags converted to strings.
+ *
+ * Given a set of flags for a given object type, return a string which
+ * can be output to a file.
+ *
+ * The returned pointer must not be freed.
+ */
 char *
 flags_to_string (FlagType flags, int object_type)
 {
@@ -573,6 +633,9 @@ flags_to_string (FlagType flags, int object_type)
 				 ENTRIES (object_flagbits));
 }
 
+/*!
+ * \brief PCB flags converted to strings.
+ */
 char *
 pcbflags_to_string (FlagType flags)
 {
