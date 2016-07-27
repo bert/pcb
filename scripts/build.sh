3aab560e5342d55e1d412cc2a3774862564d98ca
bert <bert.timmerman@xs4all.nl>
2016-07-27 19:35:07 +0200

    Travis-CI: disable docs.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ./autogen.sh
-./configure --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
+./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
 make
-./configure --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
+./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
 make
 make distcheck
 make -C tests check
