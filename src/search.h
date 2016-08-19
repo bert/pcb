40bb6da1734a6f83f99353ef47af93840a5cdf25
WileyECoyote <wileyhill@gmail.com>
2016-08-19 11:30:10 -0500

    src/search.[ch]: Bert Converted plain comments into doxygen comments

--- src/search.h
+++ src/search.h
@@ -1,30 +1,35 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/search.h
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
+ * \brief Prototypes for search routines.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <hr>
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * PCB, interactive printed circuit board design
  *
- *  Contact addresses for paper mail and Email:
- *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
- *  Thomas.Nau@rz.uni-ulm.de
+ * Copyright (C) 1994,1995,1996 Thomas Nau
  *
- */
-
-/* prototypes for search routines
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Contact addresses for paper mail and Email:
+ *
+ * Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
+ *
+ * Thomas.Nau@rz.uni-ulm.de
  */
 
 #ifndef	PCB_SEARCH_H
@@ -79,9 +84,9 @@ bool IsLineInQuadrangle (PointType p[4], LineType * Line);
 bool IsArcInRectangle (Coord, Coord, Coord, Coord, ArcType *);
 bool IsPointInPad (Coord, Coord, Coord, PadType *);
 bool IsPointInBox (Coord, Coord, BoxType *, Coord);
-int  SearchObjectByLocation (unsigned, void **, void **, void **, Coord, Coord, Coord);
-int  SearchScreen (Coord, Coord, int, void **, void **, void **);
-int  SearchObjectByID (DataType *, void **, void **, void **, int, int);
-ElementType *SearchElementByName (DataType *, char *);
+int SearchObjectByLocation (unsigned, void **, void **, void **, Coord, Coord, Coord);
+int SearchScreen (Coord, Coord, int, void **, void **, void **);
+int SearchObjectByID (DataType *, void **, void **, void **, int, int);
+ElementType * SearchElementByName (DataType *, char *);
 
 #endif
