61ca147ee2e31bfa2d49ba5b6cc343e4263d7173
Milan Prochac <milan@prochac.sk>
2016-11-06 17:05:55 +0100

    FIXED fix for crash during undo of refdes resize
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/draw.c
+++ src/draw.c
@@ -1437,8 +1437,7 @@ EraseElementName (ElementType *Element)
 {
   if (TEST_FLAG (HIDENAMEFLAG, Element))
     return;
-  DrawText (NULL, &ELEMENT_TEXT (PCB, Element));
-}
+  EraseText (NULL, &ELEMENT_TEXT (PCB, Element));}
 
 
 void
@@ -1451,8 +1450,10 @@ EraseObject (int type, void *lptr, void *ptr)
       ErasePin ((PinType *) ptr);
       break;
     case TEXT_TYPE:
-    case ELEMENTNAME_TYPE:
       EraseText ((LayerType *)lptr, (TextType *) ptr);
+      break;
+    case ELEMENTNAME_TYPE:
+      EraseElementName(lptr);
       break;
     case POLYGON_TYPE:
       ErasePolygon ((PolygonType *) ptr);
