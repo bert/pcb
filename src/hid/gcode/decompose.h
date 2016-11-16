3973a88da20d9fcc5b60515ab0bd5947c5ff166f
WileyECoyote <wileyhill@gmail.com>
2016-11-15 22:28:39 -0600

    gcode/decompose.h: Bert Converted plain comments into doxygen comments.

--- src/hid/gcode/decompose.h
+++ src/hid/gcode/decompose.h
@@ -1,6 +1,19 @@
-/* Copyright (C) 2001-2007 Peter Selinger.
-   This file is part of Potrace. It is free software and it is covered
-   by the GNU General Public License. See the file COPYING for details. */
+/*!
+ * \file src/hid/gcode/decompose.h
+ *
+ * \brief Header file for auxiliary bitmap manipulations.
+ *
+ * <hr>
+ *
+ * <h1><b>Copyright.</b></h1>\n
+ *
+ * PCB, interactive printed circuit board design
+ *
+ * Copyright (C) 2001-2007 Peter Selinger.
+ *
+ * This file is part of Potrace. It is free software and it is covered
+ * by the GNU General Public License. See the file COPYING for details.
+ */
 
 /* decompose.h 147 2007-04-09 00:44:09Z selinger */
 
