e845008419d2a2e40e46271d46f38cd97663aa8e
Peter Clifton <peter@clifton-electronics.co.uk>
2016-12-04 20:31:09 +0000

    Remove left over debug

--- src/hid/common/object3d.c
+++ src/hid/common/object3d.c
@@ -1251,8 +1251,6 @@ object3d_from_copper_layers_within_area (POLYAREA *area)
 #endif
                                 copper_appearance,
                                 NULL));
-
-      //break;
     }
 
 
