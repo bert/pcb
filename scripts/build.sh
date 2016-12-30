7af8446192fb97bf9203d73243b7c93a3ced526d
bert <bert.timmerman@xs4all.nl>
2016-12-30 18:16:19 +0100

    Travis-CI: enable generation of documentation (--enable-doc).
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ./autogen.sh
-./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
+./configure --disable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
 make
 make distclean
-./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
+./configure --disable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
 make
 
