5c7bb9bd8101e5dac3f84abb5995ae4d73d15ca0
bert <bert.timmerman@xs4all.nl>
2016-12-30 18:00:36 +0100

    Travis-CI: do not make distcheck.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,7 @@
 ./autogen.sh
 ./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
 make
-make distcheck
 make distclean
 ./configure --disable-update-desktop-database --disable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
 make
-make distcheck
+
