158af6abcd258e7195b27832e55d016051582189
bert <bert.timmerman@xs4all.nl>
2016-12-30 19:19:31 +0100

    Travis-CI: do make check.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 ./autogen.sh
 ./configure --enable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
 make
+make check
 make distclean
 ./configure --enable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
 make
+make check
 
