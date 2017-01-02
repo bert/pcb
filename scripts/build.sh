70cc99ae02f97967cd4389357ebe24d008ef6fbb
bert <bert.timmerman@xs4all.nl>
2017-01-02 22:38:06 +0100

    Travis-CI: --enable-toporouter.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 ./autogen.sh
-./configure --enable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=gtk
+./configure --enable-update-desktop-database --enable-doc --enable-dbus --enable-toporouter --enable-nls --with-gui=gtk
 make
 make check
 make distclean
-./configure --enable-update-desktop-database --enable-doc --enable-dbus --disable-toporouter --enable-nls --with-gui=lesstif
+./configure --enable-update-desktop-database --enable-doc --enable-dbus --enable-toporouter --enable-nls --with-gui=lesstif
 make
 make check
 
