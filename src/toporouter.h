77cddabd0b76c1ef62d7ebbc683a6cccb189fd67
WileyECoyote <wileyhill@gmail.com>
2017-03-11 02:41:34 -0600

    src/toporouter.h: Bert Converted plain comments into doxygen comments.

--- src/toporouter.h
+++ src/toporouter.h
@@ -1,28 +1,35 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/toporouter.h
  *
- *  Topological Autorouter for
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 2009 Anthony Blake
- *  Copyright (C) 2009-2011 PCB Contributors (see ChangeLog for details)
+ * \brief Topological Autorouter for PCB.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <hr>
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Topological Autorouter for
+ * PCB, interactive printed circuit board design
  *
- *  Contact addresses for email:
- *  Anthony Blake, tonyb33@gmail.com
+ * Copyright (C) 2009 Anthony Blake
  *
+ * Copyright (C) 2009-2011 PCB Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Contact addresses for email:
+ * Anthony Blake, tonyb33@gmail.com
  */
 
 #ifndef PCB_TOPOROUTER_H
@@ -442,13 +449,14 @@ typedef struct {
   cairo_surface_t *surface;
 #endif
 
-  double s; /* scale factor */
+  double s; /*!< scale factor. */
 
   int mode;
   void *data;
 
   char *filename;
-  double iw, ih; /* image dimensions */
+  double iw; /*!< image width dimensions. */
+  double ih; /*!< image height dimensions. */
 } drawing_context_t;
 
 #define FOREACH_CLUSTER(clusters) do { \
