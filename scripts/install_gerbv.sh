e21e098af07ec68675600b54bca9af7dbfe9dc58
Bert Timmerman <bert.timmerman@xs4all.nl>
2017-04-07 18:49:49 +0200

    Travis-CI: add an install script for gerbv
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all>

---
+++ scripts/install_gerbv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+wget https://sourceforge.net/projects/gerbv/files/gerbv/gerbv-2.6.2/gerbv-2.6.2.tar.gz/download
+tar -xzvf gerbv-2.6.2.tar.gz
+cd gerbv-2.6.2 && ./configure --prefix=/usr && make && sudo make install
