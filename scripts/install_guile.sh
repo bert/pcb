6b7ffd41b569dd29e49e730f3a30bba0eabb3aa8
bert <bert.timmerman@xs4all.nl>
2017-04-08 19:25:27 +0200

    Travis-CI: use guile version 2.0.0.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/install_guile.sh
+++ scripts/install_guile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://ftp.gnu.org/gnu/guile/guile-1.8.0.tar.gz -O guile-1.8.0.tar.gz
-tar -xzvf guile-1.8.0.tar.gz
-cd guile-1.8.0 && ./configure --prefix=/usr && make && sudo make install
+wget https://ftp.gnu.org/gnu/guile/guile-2.0.0.tar.gz -O guile-2.0.0.tar.gz
+tar -xzvf guile-2.0.0.tar.gz
+cd guile-2.0.0 && ./configure --prefix=/usr && make && sudo make install
