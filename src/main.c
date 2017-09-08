e1ec795ff9807ece840d8c6c9756a8d918160e98
bert <bert.timmerman@xs4all.nl>
2017-09-08 19:34:25 +0200

    Correction in the User Manual/General options documentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/main.c
+++ src/main.c
@@ -1558,12 +1558,12 @@ If set, all found connections are reset before a new component is scanned.
        "If set, all found connections are reset before a new component is scanned"),
 
 /* %start-doc options "1 General Options"
- * @ftable @code
- * @item --auto-buried-vias
- * Enables automatically created vias (during line moves and layer switch) to be buriad/blind vias
- * @end ftable
- * %end-doc
- * */
+@ftable @code
+@item --auto-buried-vias
+Enables automatically created vias (during line moves and layer switch) to be buriad/blind vias
+@end ftable
+%end-doc
+*/
   BSET (AutoBuriedVias, 0, "auto-buried-vias",
        "Enables automatically created vias to be buriad/blind vias"),
 
