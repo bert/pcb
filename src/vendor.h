b8f9114f5ff3cb1d577fdfdf02d61223eef94f9f
bert <bert.timmerman@xs4all.nl>
2017-11-01 21:54:22 +0100

    src/vendor.h: Converted plain comments into doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vendor.h
+++ src/vendor.h
@@ -1,23 +1,29 @@
-/*
- *                            COPYRIGHT
+/*!
+ * \file src/vendor.h
  *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 2004 Dan McMahill
+ * \brief .
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * <hr>
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * <h1><b>Copyright.</b></h1>\n
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * PCB, interactive printed circuit board design
  *
+ * Copyright (C) 2004 Dan McMahill
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef PCB_VENDOR_H
@@ -26,4 +32,4 @@
 int vendorDrillMap (int);
 bool vendorIsElementMappable (ElementType *);
 
-#endif /* __VENDOR_H__ */
+#endif /* PCB_VENDOR_H */
