e011ec097df065faa03a21bdf0edc742190b5586
bert <bert.timmerman@xs4all.nl>
2018-01-17 21:02:30 +0100

    Updated the doxygen file header.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hid/dxf/dxf.c
+++ src/hid/dxf/dxf.c
@@ -1,32 +1,5 @@
-/*
- *                            COPYRIGHT
- *
- *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
- *  Copyright (C) 1997, 1998, 1999, 2000, 2001 Harry Eaton
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *  Contact addresses for paper mail and Email:
- *  Harry Eaton, 6697 Buttonhole Ct, Columbia, MD 21044, USA
- *  haceaton@aplcomm.jhuapl.edu
- *
- */
-
 /*!
- * \file dxf.c
+ * \file src/hid/dxf/dxf.c
  *
  * \author Copyright (C) 2018 by Bert Timmerman <bert.timmerman@xs4all.nl>
  * for DXF relevant code.\n
@@ -67,9 +40,7 @@
  * <hr>
  * <p>
  * Please send remarks, bugs, improvements, relevant opinions to
- * bert.timmerman@xs4all.nl or to the geda-dev mailing list. \n
- * Donations to speed up the development of the gEDA suite best go to
- * www.gedaconsulting.com. \n
+ * bert.timmerman@xs4all.nl. \n
  * <li>Have fun ;-)
  * </ul>
  * <hr>
@@ -166,6 +137,35 @@
  * <hr>
  * \n\n
  * <b>Include files</b>
+ *
+ *
+ * <hr>
+ *
+ * <h1><b>Copyright.</b></h1>\n
+ *
+ * PCB, interactive printed circuit board design
+ * Copyright (C) 1994,1995,1996 Thomas Nau
+ * Copyright (C) 1997, 1998, 1999, 2000, 2001 Harry Eaton
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact addresses for paper mail and Email:
+ * Harry Eaton, 6697 Buttonhole Ct, Columbia, MD 21044, USA
+ * haceaton@aplcomm.jhuapl.edu
+ *
+ * <hr>
  */
 
 
