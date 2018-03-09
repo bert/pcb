93fb14d884123a5e80ba60e50d629843aa7c0bd4
bert <bert.timmerman@xs4all.nl>
2018-03-09 07:59:48 +0100

    Prevent crashes when printing with actionscript.
    Reported-by: Kai-Martin Knaak <knaak@iqo.uni-hannover.de>
    
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hid/common/hidnogui.c
+++ src/hid/common/hidnogui.c
@@ -49,7 +49,8 @@ nogui_parse_arguments (int *argc, char ***argv)
 static void
 nogui_invalidate_lr (int l, int r, int t, int b)
 {
-  CRASH;
+  printf("pcb: invalidate_lr() called without a GUI\n");
+  printf("This is ok, if you run an action script\n");
 }
 
 static void
