c743d5f5bf91a9b52939736b2550667113bb1c0b
Lambert <ljh4timm@xs4all.nl>
2019-01-11 22:14:17 +0100

    Delete install_gerbv.sh
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/install_gerbv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://sourceforge.net/projects/gerbv/files/gerbv/gerbv-2.6.2/gerbv-2.6.2.tar.gz/download -O gerbv-2.6.2.tar.gz
-tar -xzvf gerbv-2.6.2.tar.gz
-cd gerbv-2.6.2 && ./configure --prefix=/usr && make && sudo make install
