22859fd9b4b73aaee24d3a23ab2f15063c17689f
Lambert <ljh4timm@xs4all.nl>
2019-01-11 14:42:05 +0100

    Update build.sh
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+
+./autogen.sh
+./configure --enable-update-desktop-database --enable-doc --enable-dbus --enable-toporouter --enable-nls --with-gui=batch
+make
+#make check
+make clean
+
 ./autogen.sh
 ./configure --enable-update-desktop-database --enable-doc --enable-dbus --enable-toporouter --enable-nls --with-gui=gtk
 make
-make check
+#make check
+make clean
+
+#./autogen.sh
+#./configure --enable-update-desktop-database --enable-doc --enable-dbus --enable-toporouter --enable-nls --with-gui=lesstif
+#make
+#make check
+#make clean
